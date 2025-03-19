16781676b56c2373830e081c003cb98da181c597
cxxpertaylor <cooper.b.taylor@gmail.com>
2025-03-19 13:47:18 -0600

    work

--- backend/Mission11_Assignment.API/obj/Debug/net8.0/Mission11_Assignment.API.AssemblyInfo.cs
+++ backend/Mission11_Assignment.API/obj/Debug/net8.0/Mission11_Assignment.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Assignment.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6e3c92de14cec07d1bd749577652334acdb531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3b0852d5f09cbd5c048d8d20ff6726d80e808f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Assignment.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Assignment.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
